bb72966eaf881bbb147440e87fac9bea05181955
EmJhey <emjheypb@gmail.com>
2023-12-02 17:41:56 -0500

    feat: min sdk to 24; whiz to wiz; version bump;

--- app/src/main/java/com/ubasangg/mathdrills/enums/Difficulty.kt
+++ app/src/main/java/com/ubasangg/mathdrills/enums/Difficulty.kt
@@ -4,5 +4,5 @@ enum class Difficulty(val index: Int, val min: Int, val max: Int) {
     EASY(0, 0, 100),
     INTERMEDIATE(1, -100,100),
     HARD(2,-10000,10000),
-    WHIZ(3,-1000000,1000000);
+    WIZ(3,-1000000,1000000);
 }
