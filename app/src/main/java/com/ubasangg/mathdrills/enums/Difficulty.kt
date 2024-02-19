9a8440855087e72c2c809a7a88e67dea59c2d195
EmJhey <emjheypb@gmail.com>
2024-02-19 04:11:48 -0500

    added Beginner difficulty

--- app/src/main/java/com/ubasangg/mathdrills/enums/Difficulty.kt
+++ app/src/main/java/com/ubasangg/mathdrills/enums/Difficulty.kt
@@ -3,6 +3,7 @@ package com.ubasangg.mathdrills.enums
 import com.ubasangg.mathdrills.R
 
 enum class Difficulty(val index: Int, val label: Int, val mdBottom: Int, val mdTop: Int, val asMin: Int, val asMax: Int) {
+    BEGINNER(4, R.string.lbl_beginner, 10, 10, 0, 10),
     EASY(0, R.string.lbl_easy, 9, 99, 0, 100),
     INTERMEDIATE(1, R.string.lbl_intermediate, 99,99, 0, 1000),
     HARD(2, R.string.lbl_hard,  99,999, -10000, 10000),
--- app/src/main/java/com/ubasangg/mathdrills/enums/Operation.kt
+++ app/src/main/java/com/ubasangg/mathdrills/enums/Operation.kt
@@ -11,12 +11,20 @@ enum class Operation(val description: String, val index: Int)  : BinaryOperator<
             val min = difficulty.asMin
             val max = difficulty.asMax
 
+            // beginner
+            // max top number = 10, max bottom number = 10
+            // no negative
+
             // easy - answer 0 to 100
             // intermediate - answer 0 to 1,000
             // hard - answer -10,000 to 10,000
             // math whiz - answer -100,000 to 100,000
             val answer = (min..max).random()
-            val num1 = if(min == 0) (min..answer).random() else (min..max).random()
+            val num1 =
+                if(min == 0) (min..answer).random()
+                else
+                    if (answer < 0) (min..max + answer).random()
+                    else (answer..max).random()
             val num2 = answer - num1
             return Problem(num1, num2, answer)
         }
@@ -26,6 +34,11 @@ enum class Operation(val description: String, val index: Int)  : BinaryOperator<
         override fun generateProblem(difficulty: Difficulty): Problem {
             val min = difficulty.asMin
             val max = difficulty.asMax
+
+            // beginner
+            // max top number = 10, max bottom number = 10
+            // no negative
+            // minuend >= subtrahend
 
             // easy
             // answer 0 to 100
@@ -39,13 +52,8 @@ enum class Operation(val description: String, val index: Int)  : BinaryOperator<
             // math whiz - answer -100,000 to 100,000
             val answer = if(difficulty == Difficulty.INTERMEDIATE) (max * -1..max).random() else (min..max).random()
             val num1 =
-                when (difficulty) {
-                    Difficulty.EASY, Difficulty.INTERMEDIATE -> {
-                        if (answer < 0) (min..max + answer).random()
-                        else (answer..max).random()
-                    }
-                    else -> (min..max).random()
-                }
+                if (answer < 0) (min..max + answer).random()
+                else (answer..max).random()
             val num2 = num1 - answer
             return Problem(num1, num2, answer)
         }
@@ -54,14 +62,25 @@ enum class Operation(val description: String, val index: Int)  : BinaryOperator<
         override fun apply(t: Int, u: Int): Int = t * u
         override fun generateProblem(difficulty: Difficulty): Problem {
             val bottomMin =
-                if (difficulty == Difficulty.EASY) difficulty.mdBottom
-                else difficulty.mdBottom * -1
+                when(difficulty) {
+                    Difficulty.EASY, Difficulty.BEGINNER -> 0
+                    else -> difficulty.mdBottom * -1
+                }
             val bottomMax = difficulty.mdBottom
+            val whizSign = intArrayOf(-1 , 1).random()
             val topMin =
-                if (difficulty == Difficulty.EASY) 0
-                else if (difficulty == Difficulty.WHIZ) 100
-                else difficulty.mdTop * -1
-            val topMax = difficulty.mdTop
+                when(difficulty) {
+                    Difficulty.EASY, Difficulty.BEGINNER -> 0
+                    Difficulty.WHIZ -> 100 * whizSign
+                    else -> difficulty.mdTop * -1
+                }
+            val topMax =
+                if(difficulty == Difficulty.WHIZ) difficulty.mdTop * whizSign
+                else difficulty.mdTop
+
+            // beginner
+            // max top number = 10, max bottom number = 10
+            // no negative
 
             // easy
             // top max 2, and limit bottom number to 1
@@ -70,7 +89,9 @@ enum class Operation(val description: String, val index: Int)  : BinaryOperator<
             // intermediate - top max 2 digits, bottom max 2 digits
             // hard - top max 3 digits, bottom max 2 digits
             // math whiz - top to 3 digits only (100 - 999), bottom max 3 digits
-            val num1 = (topMin..topMax).random()
+            val num1 =
+                if(topMin < topMax) (topMin..topMax).random()
+                else (topMax..topMin).random()
             val num2 = (bottomMin..bottomMax).random()
             val answer = this.apply(num1, num2)
             return Problem(num1, num2, answer)
